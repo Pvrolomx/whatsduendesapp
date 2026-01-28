f8d86a9821c79c1f4e416f2b19d6d3a16a25b008
Pvrolomx <pvrolo@gmail.com>
2026-01-27 19:29:21 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' },
-    ],
-  },
+      { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' }
+    ]
+  }
 }
 module.exports = nextConfig
